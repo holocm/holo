c29bd09634c08b8135b765be3c4f7d14d2c4f207
Stefan Majewsky <majewsky@gmx.net>
2015-12-14 22:09:45 +0100

    set HOLO_ROOT_DIR to / by default, hint at future expansion
    There might be usecases for setting the HOLO_*_DIR variables to
    non-default values, for example:
    
    1. to run the same plugin multiple times, at different stages of the
       apply pipeline
    
    2. to support non-FHS-compliant distributions
    
    So we make it clear that these variables should be preferred over
    explicit use of the default values.

--- src/holo/plugins/plugin.go
+++ src/holo/plugins/plugin.go
@@ -93,6 +93,9 @@ func (p *Plugin) Command(arguments []string, stdout io.Writer, stderr io.Writer,
 	env = append(env, "HOLO_CACHE_DIR="+normalizePath(p.CacheDirectory()))
 	env = append(env, "HOLO_RESOURCE_DIR="+normalizePath(p.ResourceDirectory()))
 	env = append(env, "HOLO_STATE_DIR="+normalizePath(p.StateDirectory()))
+	if os.Getenv("HOLO_ROOT_DIR") == "" {
+		env = append(env, "HOLO_ROOT_DIR="+normalizePath(RootDirectory()))
+	}
 	cmd.Env = env
 
 	return cmd
