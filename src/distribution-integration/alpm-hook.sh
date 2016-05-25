e99e3f554a06393ec4967d4fa2803828dd76fede
Stefan Majewsky <majewsky@gmx.net>
2016-05-25 22:01:17 +0200

    do not call "holo apply" without args

--- src/distribution-integration/alpm-hook.sh
+++ src/distribution-integration/alpm-hook.sh
@@ -7,7 +7,7 @@ holo scan --short | grep '^file:' | cut -d: -f2- | sort > /tmp/files-managed-by-
 
 # changed files come from stdin (but without leading slash!); find files
 # managed by Holo among these
-sort | sed 's+^+/+' | comm -12 - /tmp/files-managed-by-holo | sed 's/^/file:/' | xargs holo apply
+sort | sed 's+^+/+' | comm -12 - /tmp/files-managed-by-holo | sed 's/^/file:/' | xargs -r holo apply
 
 # cleanup
 rm /tmp/files-managed-by-holo
