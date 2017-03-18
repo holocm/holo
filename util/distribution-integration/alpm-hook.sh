58a6627f33b471135d72dab1beda7ea59d2b0f82
Stefan Majewsky <majewsky@gmx.net>
2017-03-18 21:22:57 +0100

    sort entity list again before giving it to comm(1)
    Apparently `holo scan` sorts stuff differently than sort(1).
    In particular, I see that `holo scan --short` on one of my servers lists
    
      file:/etc/munin/munin-node.conf
      file:/etc/munin/munin.conf
    
    in that order, but `holo scan --short | sort` puts it the other way
    around.

--- util/distribution-integration/alpm-hook.sh
+++ util/distribution-integration/alpm-hook.sh
@@ -6,5 +6,5 @@ comm -12 \
      <(# changed files come from stdin (but without leading slash!)
        sort | sed 's+^+/+') \
      <(# enumerate files that are managed by holo-files
-       holo scan --short | sed -n 's/^file://p') \
+       holo scan --short | sed -n 's/^file://p' | sort) \
 | sed 's/^/file:/' | xargs -r holo apply
