9063861712ff0c9375901ded2c2a7ab06fcd0cf6
Stefan Majewsky <majewsky@gmx.net>
2017-08-02 21:51:21 +0200

    fold test/*/source trees into single files
    Also, switch to a nicer directory tree serialization format (compared to
    the old expected-tree files).

---
+++ util/dump-to-tree.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+#
+# Usage: dump-to-tree.sh <directory>
+#
+# Materializes the filesystem dump on stdin (in the format produced by
+# tree-to-dump.sh) into the given filesystem directory. The directory will be
+# created if it does not exist.
+#
+
+set -euo pipefail
+
+mkdir -p "$1"
+cd "$1"
+
+LINE_NUMBER=0
+read_and_inc() {
+  LINE_NUMBER=$((LINE_NUMBER+1))
+  read "$@"
+}
+read_and_inc_or_fail() {
+  read_and_inc "$@" || fail "unexpected end of input"
+}
+fail() {
+    echo "error: $@ on input line ${LINE_NUMBER}" >&2
+    exit 1
+}
+
+while read_and_inc FILE_TYPE FILE_MODE FILE_PATH; do
+  if [ -z "${FILE_TYPE}" -a -z "${FILE_MODE}" -a -z "${FILE_PATH}" ]; then
+    continue # ignore empty lines
+  fi
+  if [ -z "${FILE_TYPE}" -o -z "${FILE_MODE}" -o -z "${FILE_PATH}" ]; then
+    fail "unexpected end of entry header"
+  fi
+  if [[ "${FILE_PATH}" = /* ]]; then
+    fail "absolute path not allowed in entry header"
+  fi
+
+  case "${FILE_TYPE}" in
+    file)
+      install -D -m "${FILE_MODE}" /dev/null "${FILE_PATH}"
+      # header is followed by file content, terminated by a separator line like "---------------"
+      while IFS='' read_and_inc_or_fail LINE; do
+        if [[ "${LINE}" =~ ^-+$ ]]; then
+          break
+        fi
+        echo "${LINE}" >> "${FILE_PATH}"
+      done || true
+      ;;
+
+    symlink)
+      # next line contains symlink target, followed by a separator line
+      read_and_inc_or_fail TARGET
+      [ -z "${TARGET}" ] && fail "missing symlink target"
+      read_and_inc_or_fail SEPARATOR
+      [[ "${SEPARATOR}" =~ ^-+$ ]] || fail "unexpected content line, expected separator"
+      mkdir -p "$(dirname "${FILE_PATH}")"
+      ln -sf "${TARGET}" "${FILE_PATH}"
+      ;;
+
+    directory)
+      install -d -m "${FILE_MODE}" "${FILE_PATH}"
+      read_and_inc_or_fail SEPARATOR
+      [[ "${SEPARATOR}" =~ ^-+$ ]] || fail "unexpected content line, expected separator"
+      ;;
+
+    *)
+      fail "unknown file type in entry header"
+      ;;
+  esac
+done || true
---
+++ util/tree-to-dump.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# Usage: tree-to-dump.sh <directory>
+#
+# Reproducibly prints a serialization of all directories, files and symlinks
+# below <directory>, that can later be rematerialized with dump-to-tree.sh.
+#
+
+set -euo pipefail
+
+SEPARATOR="$(echo "----------------------------------------"; echo ">")"
+SEPARATOR="----------------------------------------"
+
+dump() {
+  local DIR_PATH="$1"
+  local PREFIX="$2"
+
+  for ENTRY in "${DIR_PATH}"/*; do
+    if [ -L "${ENTRY}" ]; then
+      echo "symlink   0777 ${PREFIX}/$(basename "${ENTRY}")"
+      readlink "${ENTRY}"
+      echo "${SEPARATOR}"
+
+    elif [ -f "${ENTRY}" ]; then
+      echo "file      $(stat -c 0%a "${ENTRY}") ${PREFIX}/$(basename "${ENTRY}")"
+      cat "${ENTRY}"
+      echo "${SEPARATOR}"
+
+    elif [ -d "${ENTRY}" ]; then
+      local NEXT_PREFIX="${PREFIX}/$(basename "${ENTRY}")"
+      local CONTENTS="$(dump "${ENTRY}" "${NEXT_PREFIX}")"
+      # omit directories with default mode (0755) whose existence is implied by children
+      local MODE="$(stat -c 0%a "${ENTRY}")"
+      if [ -z "${CONTENTS}" -o "${MODE}" != "0755" ]; then
+        echo "directory ${MODE} ${NEXT_PREFIX}/"
+        echo "${SEPARATOR}"
+      fi
+      if [ -n "${CONTENTS}" ]; then
+        echo "${CONTENTS}"
+      fi
+    fi
+  done
+}
+
+dump "${1:-.}" "."
